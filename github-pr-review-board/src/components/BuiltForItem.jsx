7d94321e9b1048afd27d54be817731d53ace4ffe
NadiiaLashtun <flor.smile11@gmail.com>
2025-09-12 14:51:13 +0200

    Change library for work with icons

--- github-pr-review-board/src/components/BuiltForItem.jsx
+++ github-pr-review-board/src/components/BuiltForItem.jsx
@@ -1,10 +1,15 @@
-export default function BuiltForItem({ icon: Icon, text, color }) {
+import { Icon } from "@iconify/react";
+
+export default function BuiltForItem({ icon, text, color }) {
   return (
-    <div className='p-1.5 bg-card border border-brand-primary/30 flex gap-2 items-center'>
+    <div className='p-2 bg-card border border-brand-primary/30 flex gap-2 items-center'>
       <div
         className={`w-6.5 h-6.5 flex justify-center items-center shrink-0 text-${color} border border-${color}`}
       >
-        <Icon className={`w-3.5 h-3.5`} />
+        <Icon
+          icon={icon}
+          className={`w-3.5 h-3.5`}
+        />
       </div>
       {text}
     </div>
--- github-pr-review-board/src/components/home/BuiltFor.jsx
+++ github-pr-review-board/src/components/home/BuiltFor.jsx
@@ -1,46 +1,43 @@
 import BuiltForItem from "../BuiltForItem";
 import SquareImg from "../../img/Border.png";
-import {
-  CircleStackIcon,
-  EyeIcon,
-  CodeBracketIcon,
-  ComputerDesktopIcon,
-  CpuChipIcon,
-} from "@heroicons/react/24/outline";
+import { Icon } from "@iconify/react";
 
 export default function BuiltFor() {
   const icons = [
     {
-      icon: CodeBracketIcon,
+      icon: "solar:code-bold",
       text: "GitHub API integration",
       color: " border-brand-primary/30 text-brand-primary/30",
     },
     {
-      icon: CircleStackIcon,
+      icon: "solar:database-outline",
       text: "Historical PR tracking",
       color: " border-brand/30 text-brand/30",
     },
     {
-      icon: EyeIcon,
+      icon: "solar:eye-outline",
       text: "Team-customized views",
       color: " border-brand-secondary/30 text-brand-secondary/30",
     },
     {
-      icon: ComputerDesktopIcon,
+      icon: "solar:monitor-outline",
       text: "Developer- friendly interface",
       color: " border-brand-primary/30 text-brand-primary/30",
     },
   ];
 
   return (
-    <section className='relative bg-background bg-[image:var(--gradient)] flex flex-wrap gap-9 justify-center md:justify-between pt-[84px] pb-12 px-3'>
+    <section className='relative bg-background bg-[image:var(--gradient)] flex flex-wrap gap-9 justify-evenly pt-[84px] pb-12 px-3'>
       <div
-        className='max-w-[414px] space-y-6 
+        className='max-w-[460px] space-y-6 
                       before:absolute before:top-[48px] before:left-3 before:bg-brand before:w-6 before:h-6
                       after:absolute after:top-[60px] after:left-[48px] after:right-0 after:bg-brand after:h-0.25'
       >
         <div className='absolute top-[48px] left-3 w-6 h-6 flex justify-center items-center'>
-          <CpuChipIcon className='w-4 h-4 text-white' />
+          <Icon
+            icon='solar:cpu-outline'
+            className='w-4 h-4 text-white'
+          />
         </div>
         <h2 className='text-white uppercase'>Built for Development Teams</h2>
         <p className='border-l-4 border-brand pl-3 text-text'>
