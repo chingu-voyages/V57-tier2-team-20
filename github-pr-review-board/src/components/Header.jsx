ded70f95a43d6e6c0ff98bd5a8fe045b05c5ad31
ouassima <elyakoubi.ouassima@gmail.com>
2025-09-12 13:32:19 +0100

    change icons library

--- github-pr-review-board/src/components/Header.jsx
+++ github-pr-review-board/src/components/Header.jsx
@@ -1,8 +1,7 @@
 import { NavLink } from "react-router";
 import logo from "../assets/Codesandbox.svg";
-import { House, FileSend, FileCheck, ClockCircle, HamburgerMenu } from "@solar-icons/react";
+import { Icon } from "@iconify/react";
 import { useState } from "react";
-import { CloseSquare } from '@solar-icons/react'
 
 export default function Header() {
   const today = new Date();
@@ -16,7 +15,7 @@ export default function Header() {
 
   return (
     <header className="bg-[#141923] bg-[linear-gradient(90deg,rgba(1,255,255,0.05)_0%,rgba(1,255,255,0)_50%,rgba(255,0,128,0.05)_100%)] text-white border-t-8 border-transparent [border-image:linear-gradient(to_right,#00FFFF,#8A2BE2,#FF0080)_1] ">
-      <nav className="flex justify-between items-center items-stretch px-4 py-2 border-b-2 border-[#01FFFF33] relative">
+      <nav className="flex justify-between  items-stretch px-4 py-2 border-b-2 border-[#01FFFF33] relative">
 
         {/* Logo + Title */}
         <div className="flex items-center gap-2">
@@ -35,15 +34,19 @@ export default function Header() {
         {/* Hamburger button (mobile only) */}
         <div className="md:hidden flex items-center ">
         {isOpen ? 
-            <CloseSquare weight="Outline" size={30} color="#00ffff"            
-                onClick={() => setIsOpen(!isOpen) }
-            />:
+            <Icon 
+              color="#00ffff" 
+              icon="solar:close-square-outline" 
+              width="24" 
+              height="24" 
+              onClick={() => setIsOpen(!isOpen) } 
+            /> :
             <div className="border-1 p-1 border-[#01ffff33]">
-                <HamburgerMenu
-                    weight="Broken"
-                    size={30}
-                    color="#00FFFF"
-                    onClick={() => setIsOpen(!isOpen) }
+                <Icon  
+                  icon="solar:hamburger-menu-broken" 
+                  width="24" height="24"
+                  onClick={() => setIsOpen(!isOpen) }
+                  color="#00FFFF"
                 />
             </div>
           }
@@ -53,14 +56,14 @@ export default function Header() {
         <div
           className={`
             ${isOpen ? "flex flex-col items-stretch w-full p-2 " : "hidden md:flex"}
-            md:flex-row md:items-center md:items-stretch md:p-0 md:m-0
+            md:flex-row  md:items-stretch md:p-0 md:m-0
             absolute md:static top-full left-0 w-full md:w-auto md: bg-[#141923] md:bg-transparent
             z-50
           `}
         >
           {/* Mobile date (only visible on mobile when menu is open) */}
           <div className="flex items-center justify-center text-[#01FFFF33] mb-2 md:hidden border-1 border-[#01FFFF33] px-4 py-2">
-            <ClockCircle weight="Outline" size={30} />
+            <Icon icon="solar:clock-circle-linear" width="24" height="24" />
             <p className="uppercase text-sm ml-2">{formattedDate}</p>
           </div>
 
@@ -68,12 +71,12 @@ export default function Header() {
           <NavLink
             to="."
             className={({ isActive }) =>
-              `flex items-center justify-center gap-2 justify-center transition-colors duration-200 border-1 border-[#01FFFF33] px-4 py-2 ${
+              `flex items-center justify-center gap-2 transition-colors duration-200 border-1 border-[#01FFFF33] px-4 py-2 ${
                 isActive ? "bg-[#00FFFF] text-[#1E232D] shadow" : "hover:bg-[#01FFFF]/60 text-white"
               }`
             }
           >
-            <House weight="Outline" size={20} />
+            <Icon icon="solar:home-outline" width="24" height="24" />            
             Home
           </NavLink>
 
@@ -85,7 +88,7 @@ export default function Header() {
               }`
             }
           >
-            <FileSend weight="Outline" size={20} />
+            <Icon icon="solar:file-send-outline" width="24" height="24" />
             Open PRs
           </NavLink>
 
@@ -97,13 +100,13 @@ export default function Header() {
               }`
             }
           >
-            <FileCheck weight="Outline" size={20} />
+            <Icon icon="solar:file-check-outline" width="24" height="24" />
             Closed PRs
           </NavLink>
         </div>
          {/* Desktop date (always visible on md+) */}
         <div className="hidden md:flex items-center text-[#01FFFF33] border-1 border-[#01FFFF33] px-4 py-2 ml-4">
-          <ClockCircle weight="Outline" size={30} />
+          <Icon icon="solar:clock-circle-outline" width="24" height="24" />          
           <p className="uppercase text-sm ml-2">{formattedDate}</p>
         </div>
       </nav>
