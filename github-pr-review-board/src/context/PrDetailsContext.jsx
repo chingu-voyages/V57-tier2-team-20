03b5691ee9d714fa14db419dd50e34dce5a940c6
Alexander <159818223+Alexander-NM@users.noreply.github.com>
2025-10-01 21:29:50 +0300

    Added current org and repo info to local storage for further initialization at startup.

--- github-pr-review-board/src/context/PrDetailsContext.jsx
+++ github-pr-review-board/src/context/PrDetailsContext.jsx
@@ -1,21 +1,27 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from "react"
 
-const PrDetailsContext = createContext(null);
+const PrDetailsContext = createContext(null)
 
 export function PrDetailsProvider({ children }) {
-  const [newPrDetails, setNewPrDetails] = useState({});
+    const [newPrDetails, setNewPrDetails] = useState(() => {
+        const currentOrgRepo = JSON.parse(localStorage.getItem("currentOrgRepo") || "{}")
+        if (currentOrgRepo.orgName && currentOrgRepo.repoName) {
+            return { ...currentOrgRepo }
+        }
+        return {}
+    })
 
-  return (
-    <PrDetailsContext.Provider value={{ newPrDetails, setNewPrDetails }}>
-      {children}
-    </PrDetailsContext.Provider>
-  );
+    return (
+        <PrDetailsContext.Provider value={{ newPrDetails, setNewPrDetails }}>
+            {children}
+        </PrDetailsContext.Provider>
+    )
 }
 
 export function usePrDetails() {
-  const context = useContext(PrDetailsContext);
-  if (!context) {
-    throw new Error("usePrDetails must be used within a PrDetailsProvider");
-  }
-  return context;
+    const context = useContext(PrDetailsContext)
+    if (!context) {
+        throw new Error("usePrDetails must be used within a PrDetailsProvider")
+    }
+    return context
 }
