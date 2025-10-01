b5adc2dfdb7214455c6eec378b1e0a8722612ee7
NadiiaLashtun <flor.smile11@gmail.com>
2025-10-01 15:04:53 +0200

    Change Message and Button parameters

--- github-pr-review-board/src/App.jsx
+++ github-pr-review-board/src/App.jsx
@@ -20,33 +20,37 @@ function App() {
           />
           <Route
             path='open-prs'
-            element={<PRStartScreen 
-            title="Open PRs Dashboard"
-            description="Initialize a repository from the home page to view open pull"
-            icon="solar:folder-open-outline"
-            EmptyComponent={OpenPRs}
-            theme={{
-              primary: "bg-brand-primary",
-              icon: "text-white",
-              text: "text-white",
-              border: "border-brand-primary/20"
-            }}
-            />}
+            element={
+              <PRStartScreen
+                title='Open PRs Dashboard'
+                description='Initialize a repository from the home page to view open pull'
+                icon='solar:folder-open-outline'
+                EmptyComponent={OpenPRs}
+                theme={{
+                  primary: "bg-brand-primary",
+                  icon: "text-text",
+                  text: "text-text",
+                  border: "border-brand-primary/20",
+                }}
+              />
+            }
           />
           <Route
             path='closed-prs'
-            element={<PRStartScreen 
-            title="Closed Pull Requests"
-            description="Initialize a repository from the home page to view closed pull requests"
-            icon="solar:folder-check-outline"
-            EmptyComponent={ClosedPRs}
-            theme={{
-              primary: "bg-brand-secondary",
-              icon: "text-brand-secondary", // bright pink
-              text: "text-brand-secondary",
-              border: "border-brand-secondary/20"
-            }}
-            />}
+            element={
+              <PRStartScreen
+                title='Closed Pull Requests'
+                description='Initialize a repository from the home page to view closed pull requests'
+                icon='solar:folder-check-outline'
+                EmptyComponent={ClosedPRs}
+                theme={{
+                  primary: "bg-brand-secondary",
+                  icon: "text-brand-secondary", // bright pink
+                  text: "text-brand-secondary",
+                  border: "border-brand-secondary/20",
+                }}
+              />
+            }
           />
         </Route>
         <Route
--- github-pr-review-board/src/components/PrimaryButton.jsx
+++ github-pr-review-board/src/components/PrimaryButton.jsx
@@ -1,10 +1,16 @@
 import { Icon } from "@iconify/react";
 
-export default function Button1({ icon, text, onClick, className =""}) {
+export default function Button1({
+  icon,
+  text,
+  color,
+  onClick,
+  className = "",
+}) {
   return (
     <button
-      className={`flex items-center gap-2 bg-brand-primary text-muted px-4 py-2.5 cursor-pointer transition duration-500 hover:bg-background hover:text-brand-primary hover:border hover:border-brand-primary ${className}`}  
-          onClick={onClick}
+      className={`flex items-center gap-2 ${color} text-mutted px-4 py-2.5 cursor-pointer transition duration-500 hover:bg-background hover:text-brand-primary border hover:border-brand-primary ${className} `}
+      onClick={onClick}
     >
       <Icon
         icon={icon}
