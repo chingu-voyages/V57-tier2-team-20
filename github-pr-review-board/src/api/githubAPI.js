169d615fa995f1ca4990bbee08186c646c3e9e33
NadiiaLashtun <flor.smile11@gmail.com>
2025-10-07 15:11:58 +0200

    Fix PR status. Uncomment activities

--- github-pr-review-board/src/api/githubAPI.js
+++ github-pr-review-board/src/api/githubAPI.js
@@ -4,12 +4,14 @@ async function fetchAPI(url) {
   try {
     // const response = await octokit.request(url);
     const response = await fetch(
-        `https://github-pr-board.backend-iaas.workers.dev/?url=${encodeURIComponent(url)}`
+      `https://github-pr-board.backend-iaas.workers.dev/?url=${encodeURIComponent(
+        url
+      )}`
     );
     if (!response.ok) {
-        const error = new Error(response.statusText)
-        error.status = response.status
-        throw error
+      const error = new Error(response.statusText);
+      error.status = response.status;
+      throw error;
     }
     const data = await response.json();
     return data;
@@ -55,58 +57,60 @@ export async function getPullRequests(org, repo, state) {
   );
 
   //Without activities
-  return info.map((pr) => ({
-    title: pr.title,
-    number: pr.number,
-    pr_url: pr.html_url,
-    created_at: timeAgo(pr.created_at),
-    author: pr.user.login,
-    authorUrl: `https://github.com/${pr.user.login}`,
-    fromBranch: pr.head.ref,
-    fromBranchUrl: `${pr.base.repo.html_url}/tree/${pr.head.ref}`,
-    toBranch: pr.base.ref,
-    toBranchUrl: `${pr.base.repo.html_url}/tree/${pr.base.ref}`,
-    orgName: pr.base.repo.owner.login,
-    orgUrl: pr.base.repo.owner.html_url,
-    repoName: pr.base.repo.name,
-    repoUrl: pr.base.repo.html_url,
-    reviewers: pr.requested_reviewers?.length
-      ? pr.requested_reviewers.map((user) => ({
-          login: user.login,
-          url: `https://github.com/${user.login}`,
-        }))
-      : [],
-    activities: null,
-    state:pr.state,
-    merged:pr.merged_at
-  }));
+  // return info.map((pr) => ({
+  //   title: pr.title,
+  //   number: pr.number,
+  //   pr_url: pr.html_url,
+  //   created_at: timeAgo(pr.created_at),
+  //   author: pr.user.login,
+  //   authorUrl: `https://github.com/${pr.user.login}`,
+  //   fromBranch: pr.head.ref,
+  //   fromBranchUrl: `${pr.base.repo.html_url}/tree/${pr.head.ref}`,
+  //   toBranch: pr.base.ref,
+  //   toBranchUrl: `${pr.base.repo.html_url}/tree/${pr.base.ref}`,
+  //   orgName: pr.base.repo.owner.login,
+  //   orgUrl: pr.base.repo.owner.html_url,
+  //   repoName: pr.base.repo.name,
+  //   repoUrl: pr.base.repo.html_url,
+  //   reviewers: pr.requested_reviewers?.length
+  //     ? pr.requested_reviewers.map((user) => ({
+  //         login: user.login,
+  //         url: `https://github.com/${user.login}`,
+  //       }))
+  //     : [],
+  //   activities: null,
+  //   state:pr.state,
+  //   merged:pr.merged_at
+  // }));
 
   //With activities
-  //   return Promise.all(
-  //     info.map(async (pr) => ({
-  //       title: pr.title,
-  //       number: pr.number,
-  //       pr_url: pr.html_url,
-  //       created_at: timeAgo(pr.created_at),
-  //       author: pr.user.login,
-  //       authorUrl: `https://github.com/${pr.user.login}`,
-  //       fromBranch: pr.head.ref,
-  //       fromBranchUrl: `${pr.base.repo.html_url}/tree/${pr.head.ref}`,
-  //       toBranch: pr.base.ref,
-  //       toBranchUrl: `${pr.base.repo.html_url}/tree/${pr.base.ref}`,
-  //       orgName: pr.base.repo.owner.login,
-  //       orgUrl: pr.base.repo.owner.html_url,
-  //       repoName: pr.base.repo.name,
-  //       repoUrl: pr.base.repo.html_url,
-  //       reviewers: pr.requested_reviewers?.length
-  //         ? pr.requested_reviewers.map((user) => ({
-  //             login: user.login,
-  //             url: `https://github.com/${user.login}`,
-  //           }))
-  //         : [],
-  //       activities: await getPullRequestEvents(org, repo, pr.number),
-  //     }))
-  //   );
+  return Promise.all(
+    info.map(async (pr) => ({
+      title: pr.title,
+      number: pr.number,
+      pr_url: pr.html_url,
+      created_at: timeAgo(pr.created_at),
+      author: pr.user.login,
+      authorUrl: `https://github.com/${pr.user.login}`,
+      fromBranch: pr.head.ref,
+      fromBranchUrl: `${pr.base.repo.html_url}/tree/${pr.head.ref}`,
+      toBranch: pr.base.ref,
+      toBranchUrl: `${pr.base.repo.html_url}/tree/${pr.base.ref}`,
+      orgName: pr.base.repo.owner.login,
+      orgUrl: pr.base.repo.owner.html_url,
+      repoName: pr.base.repo.name,
+      repoUrl: pr.base.repo.html_url,
+      reviewers: pr.requested_reviewers?.length
+        ? pr.requested_reviewers.map((user) => ({
+            login: user.login,
+            url: `https://github.com/${user.login}`,
+          }))
+        : [],
+      activities: await getPullRequestEvents(org, repo, pr.number),
+      state: pr.state,
+      merged: pr.merged_at,
+    }))
+  );
 }
 
 //Get last 3 events
